e03732324548fe61d2e48d26961a1b39489e27af
Michael Nutt <michael@nuttnet.net>
2026-03-08 19:30:28 -0400

    sandstorm runtime: configure celery broker/backend and state dir

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -4,12 +4,15 @@ set -euo pipefail
 
 export HOME=/var
 export TMPDIR=/var/tmp
+VENV=/opt/app-venv
+export PATH="$VENV/bin:$PATH"
 
 rm -rf $TMPDIR
 mkdir -p $TMPDIR
 mkdir -p /var/log
 rm -rf /var/run
 mkdir -p /var/run
+mkdir -p /var/celery
 test -e /var/mediagoblin.ini || cp /opt/app/mediagoblin_local.ini /var/mediagoblin.ini
 mkdir -p /var/user_dev
 mkdir -p /var/user_dev/media/public/media_entries && mkdir -p /var/user_dev/media/queue/media_entries
