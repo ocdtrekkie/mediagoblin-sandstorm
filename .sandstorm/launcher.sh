2267254a3b721674a8413df725ce23c6dde75d12
Jacob Weisz <inbox@jacobweisz.com>
2026-04-01 22:45:26 -0500

    Attempt on fixing updates

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -10,6 +10,7 @@ export LC_ALL=C.UTF-8
 VENV=/opt/app-venv
 export PATH="$VENV/bin:$PATH"
 
+rm -rf /var/lock
 rm -rf $TMPDIR
 mkdir -p $TMPDIR
 mkdir -p /var/log
@@ -21,8 +22,9 @@ mkdir -p /var/user_dev
 mkdir -p /var/user_dev/media/public/media_entries && mkdir -p /var/user_dev/media/queue/media_entries
 
 # Version migration
-test -e /var/VERSION || echo "0.7.0" > /var/VERSION
-[[ "$(cat /var/VERSION)" == "0.15.0" ]] || (cd /opt/mediagoblin && echo "Upgrading Database...." && ./bin/gmg dbupdate && echo "0.15.0" > /var/VERSION)
+test -e /var/VERSION || touch /var/VERSION
+[[ "$(cat /var/VERSION)" == "0.7.1" ]] && (cd /opt/mediagoblin && ./bin/gmg alembic current && echo "Stamping Database...." && ./bin/gmg alembic stamp 52bf0ccbedc1)
+[[ "$(cat /var/VERSION)" == "0.15.0" ]] || (cd /opt/mediagoblin && echo "Upgrading Database...."  && ./bin/gmg dbupdate && echo "0.15.0" > /var/VERSION)
 
 cd /opt/mediagoblin
 ./lazyserver.sh --server-name=broadcast 2>&1
